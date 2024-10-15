25ae824916ff22d8d65f532631aa621e5b19e250
Mariusz <pinkiweb88@gmail.com>
2024-10-15 06:27:01 +0200

    created function to request api and get content json, created info location and info_waste_flows variable

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
 import requests
 import json
 
+main_url = 'https://cyclusnv.nl'
+
 #The data to be sent in the GET request
 data = {
     'postcode': '2861XH',
     'huisnummer': '10'         
 }
 
-# The URL of the endpoint to which you want to send data
-main_url = 'https://cyclusnv.nl'
-url = main_url + '/adressen/' + data["postcode"] +":"+ data["huisnummer"]
+def get_content_json(url):
+    # Send request GET
+    response = requests.get(url)
+    # Checking the response from the server
+    if response.status_code == 200:
+        content_json = json.loads(response.text) # Parsing a JSON string to a Python object
+        return content_json
+    else:
+        print(f"An error occurred: {response.status_code}")
 
 
 
-# Send request GET
-# response = requests.get(url, data=data)
-response = requests.get(url)
+info_location = get_content_json(main_url + '/adressen/' + data["postcode"] +":"+ data["huisnummer"])
+print(info_location)
 
 
-# Checking the response from the server
-if response.status_code == 200:
-    print("Response server:")
-    print(response.text) # Display the content of the response
-    content_json = json.loads(response.text) # Parsing a JSON string to a Python object
-    bag_id = content_json[0]['bagid'] # we ned id Basisregistratie Adressen en Gebouwen (BAG)
-else:
-    print(f"An error occurred: {response.status_code}")
+bag_id = info_location[0]['bagid'] # we need id Basisregistratie Adressen en Gebouwen (BAG) to next request
 print(bag_id)
+# content info about garbage flows
+info_waste_flows = get_content_json(main_url + '/rest/adressen/' + bag_id +"/afvalstromen")
+print(info_waste_flows)
+
