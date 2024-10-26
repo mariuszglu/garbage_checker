38a4df337959c9f8e99dbe03037f726edbf05483
Mariusz <pinkiweb88@gmail.com>
2024-10-26 21:41:53 +0200

    changed declaration structure, cleaning code

--- garbage_checker.c
+++ garbage_checker.c
@@ -1,33 +1,32 @@
 #include <stdio.h>
 #include <string.h>
+#include <curl/curl.h>
+#include <cjson/cJSON.h>
 
 //Structure definition
-struct Adres {
+typedef struct{
     char postcode[10]; //We use the character table for the postal code
     char housenumber[5]; //We use the character table for the house number
     char main_url[20]; //We use the character table for the URL (https://cyclusnv.nl)
     char bag_id[20]; //We need id Basisregistratie Adressen en Gebouwen (BAG) to next request
-    
+} Adres;
 
-};
-
-char imie[10];
 
 int main(void) {
  
-    //Declaration of a struct variable Address
-    struct Adres data;
+    //Tworzenie instancji struktury Address
+    Adres address;
     
 
-    strcpy(data.postcode, "2867CG");
-    strcpy(data.housenumber, "8");
-    strcpy(data.main_url, "https://cyclusnv.nl");
+    strcpy(address.postcode, "2867CG");
+    strcpy(address.housenumber, "8");
+    strcpy(address.main_url, "https://cyclusnv.nl");
 
 
     //Displaying content
-    printf("Postcode: %s\n", data.postcode);
-    printf("Number house: %s\n", data.postcode);
-    printf("URL: %s\n", data.main_url);
+    printf("Postcode: %s\n", address.postcode);
+    printf("Number house: %s\n", address.postcode);
+    printf("URL: %s\n", address.main_url);
    
 
     return 0;
